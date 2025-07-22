6f5a1c7cc4e1d3e16422b772f8961258f0d04e64
Harshavardhanreddy2004 <katlaharshavardhanreddy@gmail.com>
2025-07-22 14:30:48 +0530

    first version upto evalutae

---
+++ venv/mlp_cifar.py
@@ -0,0 +1,30 @@
+from keras.models import Sequential
+from keras.layers import Dense,Flatten
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+#import data
+(X_train,y_train),(X_test,y_test)=cifar10.load_data()
+
+#Categorical
+y_train=to_categorical(y_train)
+y_test=to_categorical(y_test)
+
+#build the architecture
+model=Sequential()
+model.add(Flatten(input_shape=(32,32,3)))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+
+#compile 
+model.compile(optimizer='adam',loss='categorical_crossentropy')
+
+#Train
+model.fit(X_train,y_train,epochs=10,batch_size=64)
+
+#Evalute
+model.evaluate(X_test,y_test)
+
+#
+
+
+
